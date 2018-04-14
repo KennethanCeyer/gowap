cf8d8309f15a370ece2073b4bdb331ff7219aef4
PIGNOSE <kennethan@nhpcw.com>
2018-04-15 04:46:01 +0900

    WIP: fix coverage profile issue

--- .travis.test.sh
+++ .travis.test.sh
@@ -5,8 +5,8 @@ echo "" > coverage/coverage.out
 
 for d in $(go list ./... | grep -v vendor); do
     go test -race -v -coverprofile=coverage.chunk.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat coverage.chunk.out >> coverage.out
+    if [ -f coverage.chunk.out ]; then
+        cat coverage.chunk.out >> coverage/coverage.out
         rm coverage.chunk.out
     fi
 done
