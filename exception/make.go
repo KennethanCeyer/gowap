004f16d6675fe54e42dda74649d5e55b2c7707de
PIGNOSE <kennethan@nhpcw.com>
2018-04-16 00:25:01 +0900

    WIP: add test code for make.Error

--- exception/make.go
+++ exception/make.go
@@ -2,8 +2,8 @@ package exception
 
 import "fmt"
 
-// Make is generate new Error to using Pair struct
+// Error is generate new Error to using Pair struct
 func Error(p Pair, a ...interface{}) error {
-	return fmt.Errorf(fmt.Sprintf("exception(%d): %s", p.Code, p.Message), a)
+	return fmt.Errorf(fmt.Sprintf("exception(%d): %s", p.Code, p.Message), a...)
 }
 
--- exception/make_test.go
+++ exception/make_test.go
@@ -1,1 +1,13 @@
 package exception
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMake(t *testing.T) {
+	var testError = Pair { "hello", 0x0001 }
+	var makeError = Error(testError)
+
+	assert.Equal(t, "exception(1): hello", makeError.Error())
+}
