63d5e51fbcc82784c22959af488132597e7d4908
Kenneth Ceyer <kennethan@nhpcw.com>
2018-04-15 21:50:58 +0900

    WIP: improve test script

--- .travis.test.sh
+++ .travis.test.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage/coverage.out
+
+DIRECTORY="coverage"
+
+if [ ! -d $DIRECTORY ]; then
+    mkdir $DIRECTORY
+fi
+
+echo "mode: set" > coverage/coverage.out
 
 for d in $(go list ./... | grep -v vendor); do
     go test -race -v -coverprofile=coverage.chunk.out -covermode=atomic $d
     if [ -f coverage.chunk.out ]; then
-        cat coverage.chunk.out >> coverage/coverage.out
+        grep -h -v "^mode:" coverage.chunk.out >> coverage/coverage.out
         rm coverage.chunk.out
     fi
 done
 
 cat coverage/coverage.out > c.out
+rm -rf $DIRECTORY
